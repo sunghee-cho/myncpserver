97de3116dbcedacbfed9c905c5c3831be214c05e
Notebook9 <bluejeansh@hanmail.net>
2024-06-28 15:07:48 +0900

    uploadinform modify

--- demo/src/main/java/upload/UploadInform.java
+++ demo/src/main/java/upload/UploadInform.java
@@ -1,6 +1,8 @@
 package upload;
 
 public class UploadInform {
+	//로컬의 경로와 url 매핑시
+	//-ncp서버에서는 변경 필요
 	public static final String uploadPath = "c:/fullstack/upload/";
 	public static final String uploadLocation = "file:///c:/fullstack/upload/";
 }
